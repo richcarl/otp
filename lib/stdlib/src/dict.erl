f56a282bd6375958cd3d1b92d7d4a0f77a8cc8f3
Richard Carlsson <richardc@klarna.com>
2011-08-10 16:02:11 +0200

    completed implementation of unified dict interface

--- lib/stdlib/src/dict.erl
+++ lib/stdlib/src/dict.erl
@@ -43,17 +43,17 @@
 -export([from_list/1,from_list/2,from_orddict/1,from_orddict/2]).
 -export([get/2,get/3,find/2,keys/1,values/1,values/2,erase/2]).
 -export([store/3,replace/3]).
--export([append/3,append_list/3,update/3,update/4,increment/3]).
+-export([append/3,append_list/3,increment/3]).
 -export([foreach/2,map/2,map/3,map/4,filter/2,merge/3]).
 -export([foldl/3,foldr/3,foldl1/2,foldr1/2,foldln/3,foldrn/3]).
 -export([first_key/1,last_key/1,next_key/2,prev_key/2]).
 -export([take_first/1,take_last/1,take/2]).
 
 %% Deprecated interface
--export([fetch/2,fetch_keys/1,update_counter/3,fold/3]).
+-export([fetch/2,fetch_keys/1,update/3,update/4,update_counter/3,fold/3]).
 
 %% Low-level interface.
-%%-export([get_slot/2,get_bucket/2,on_bucket/3,fold_dict/3,
+%%-export([get_slot/2,get_bucket/2,on_bucket/3,foldl/3,
 %%	 maybe_expand/2,maybe_contract/2]).
 
 %% This module also defines the standard dict behaviour.
@@ -140,7 +140,8 @@ find_key(_, []) -> false.
       List :: [{Key :: term(), Value :: term()}].
 
 to_list(D) ->
-    fold(fun (Key, Val, List) -> [{Key,Val}|List] end, [], D).
+    %% list in default traversal order, hence foldr, not foldl
+    foldr(fun (Key, Val, List) -> [{Key,Val}|List] end, [], D).
 
 to_orddict(D) ->
     lists:keysort(1, to_list(D)).
@@ -149,6 +150,7 @@ to_orddict(D) ->
       List :: [{Key :: term(), Value :: term()}],
       Dict :: dict().
 
+%% note that for duplicate keys, later entries take precedence here
 from_list(L) ->
     lists:foldl(fun ({K,V}, D) -> store(K, V, D) end, new(), L).
 
@@ -169,20 +171,36 @@ size(#dict{size=N}) when is_integer(N), N >= 0 -> N.
 is_empty(Dict) -> size(Dict) =< 0.
 
 info(Dict) ->
-    Items = [],
+    Items = [size],
     [info(Item, Dict) || Item <- Items].
 
-info(_Item, _Dict) -> throw('FIXME').
+info(size, Dict) -> size(Dict).
 
-values(_Dict) -> throw('FIXME').
+%% result is ordered (by keys) only if dict is ordered
+values(Dict) ->
+    %% list in default traversal order, hence foldr, not foldl
+    foldr(fun (_Key, Val, Acc) -> [Val|Acc] end, [], Dict).
 
-values(_Key, _Dict) -> throw('FIXME').
+%% a variant of find/2 that returns a list of values, possibly empty
+-spec values(Key, Dict) -> [Value] | [] when
+      Key :: term(),
+      Dict :: dict(),
+      Value :: term().
+values(Key, Dict) ->
+    Slot = get_slot(Dict, Key),
+    Bkt = get_bucket(Dict, Slot),
+    values_1(Key, Bkt).
+
+values_1(K, [?kv(K,Val)|_]) -> [Val];
+values_1(K, [_|Bkt]) -> find_val(K, Bkt);
+values_1(_, []) -> [].
 
 -spec fetch(Key, Dict) -> Value when
       Key :: term(),
       Dict :: dict(),
       Value :: term().
 
+%% deprecated version of get/2
 fetch(Key, D) ->
     get(Key, D).
 
@@ -221,28 +239,28 @@ find_val(K, [?kv(K,Val)|_]) -> {ok,Val};
 find_val(K, [_|Bkt]) -> find_val(K, Bkt);
 find_val(_, []) -> error.
 
-keys(Dict) -> fetch_keys(Dict).
+-spec take(Key, Dict0) -> {Value, Dict1} | 'error' when
+      Key :: term(),
+      Dict0 :: dict(),
+      Dict1 :: dict(),
+      Value :: term().
+take(Key, Dict) ->
+    case find(Key, Dict) of
+        error -> error;
+        {ok,Val} -> {Val, erase(Key,Dict)}
+    end.
 
--spec fetch_keys(Dict) -> Keys when
+%% deprecated version of keys/1
+fetch_keys(Dict) -> keys(Dict).
+
+-spec keys(Dict) -> Keys when
       Dict :: dict(),
       Keys :: [term()].
 
-fetch_keys(D) ->
-    fold(fun (Key, _Val, Keys) -> [Key|Keys] end, [], D).
-
-first_key(_Dict) -> throw('FIXME').
-
-last_key(_Dict) -> throw('FIXME').
-
-next_key(_Key, _Dict) -> throw('FIXME').
-
-prev_key(_Key, _Dict) -> throw('FIXME').
-
-take_first(_Dict) -> throw('FIXME').
-
-take_last(_Dict) -> throw('FIXME').
-
-take(_Key, _Dict) -> throw('FIXME').
+%% result is ordered only if dict is ordered
+keys(D) ->
+    %% list in default traversal order, hence foldr, not foldl
+    foldr(fun (Key, _Val, Keys) -> [Key|Keys] end, [], D).
 
 -spec erase(Key, Dict1) -> Dict2 when
       Key :: term(),
@@ -329,67 +347,109 @@ app_list_bkt(Key, L, [Other|Bkt0]) ->
     {[Other|Bkt1],Ic};
 app_list_bkt(Key, L, []) -> {[?kv(Key,L)],1}.
 
-%% %% first_key(Table) -> {ok,Key} | error.
-%% %%  Find the "first" key in a Table.
+%% first_key(Table) -> {ok,Key} | error.
+%% Find the "first" key in a Table (in the order defined by foldl).
 
-%% first_key(T) ->
-%%     case next_bucket(T, 1) of
-%% 	[?kv(K,Val)|Bkt] -> {ok,K};
-%% 	[] -> error				%No elements
-%%     end.
+first_key(T) ->
+    case next_bucket(T, T#dict.n) of
+ 	[?kv(K,_Val)|_Bkt] -> {ok,K};
+ 	[] -> error				%No elements
+    end.
 
-%% next_bucket(T, Slot) when Slot > T#dict.n -> [];
-%% next_bucket(T, Slot) ->
-%%     case get_bucket(T, Slot) of
-%% 	[] -> next_bucket(T, Slot+1);		%Empty bucket
-%% 	B -> B
-%%     end.
+%% see foldl for details on why this goes from higher bucket numbers to lower
+next_bucket(_T, Slot) when Slot < 1 -> [];
+next_bucket(T, Slot) ->
+    case get_bucket(T, Slot) of
+ 	[] -> next_bucket(T, Slot-1);		%Empty bucket
+ 	B -> B
+    end.
 
-%% %% next_key(Table, Key) -> {ok,NextKey} | error.
+take_first(Dict) ->
+    take(first_key(Dict), Dict).
 
-%% next_key(T, Key) ->
-%%     Slot = get_slot(T, Key),
-%%     B = get_bucket(T, Slot),
-%%     %% Find a bucket with something in it.
-%%     Bkt = case bucket_after_key(Key, B) of
-%% 	      no_key -> exit(badarg);
-%% 	      [] -> next_bucket(T, Slot+1);
-%% 	      Rest -> Rest
-%% 	  end,
-%%     case Bkt of
-%% 	[?kv(Next,Val)|_] -> {ok,Next};
-%% 	[] -> error				%We have reached the end!
-%%     end.
+%% next_key(Key, Table) -> {ok,NextKey} | error.
+%% Find next key *following* Key in Table (in the order defined by foldl).
 
-%% bucket_after_key(Key, [?kv(Key,Val)|Bkt]) -> Bkt;
-%% bucket_after_key(Key, [Other|Bkt]) ->
-%%     bucket_after_key(Key, Bkt);
-%% bucket_after_key(Key, []) -> no_key.		%Key not found!
+next_key(Key, T) ->
+    Slot = get_slot(T, Key),
+    B = get_bucket(T, Slot),
+    %% Find a bucket with something in it.
+    Bkt = case bucket_after_key(Key, B) of
+ 	      error -> erlang:error(badarg,[Key, T]);
+ 	      [] -> next_bucket(T, Slot-1);
+ 	      Found -> Found
+ 	  end,
+    case Bkt of
+ 	[?kv(Next,_Val)|_] -> {ok,Next};
+ 	[] -> error				%We have reached the end!
+    end.
 
-%% %% on_key(Fun, Key, Dictionary) -> Dictionary.
+bucket_after_key(Key, [?kv(Key,_Val)|Bkt]) -> Bkt;
+bucket_after_key(Key, [_Other|Bkt]) ->
+    bucket_after_key(Key, Bkt);
+bucket_after_key(_Key, []) -> error.		%Key not found!
 
-%% on_key(F, Key, D0) ->
-%%     Slot = get_slot(D0, Key),
-%%     {D1,Dc} = on_bucket(fun (B0) -> on_key_bkt(Key, F, B0) end,
-%% 			D0, Slot),
-%%     maybe_contract(D1, Dc).
+%% last_key(Table) -> {ok,Key} | error.
+%% Find the "last" key in a Table (in the order defined by foldl).
 
-%% on_key_bkt(Key, F, [?kv(Key,Val)|Bkt]) ->
-%%     case F(Val) of
-%% 	{ok,New} -> {[?kv(Key,New)|Bkt],0}; 
-%% 	erase -> {Bkt,1}
-%%     end;
-%% on_key_bkt(Key, F, [Other|Bkt0]) ->
-%%     {Bkt1,Dc} = on_key_bkt(Key, F, Bkt0),
-%%     {[Other|Bkt1],Dc}.
+last_key(T) ->
+    case prev_bucket(T, 1) of
+ 	[] -> error;				%No elements
+ 	Bkt ->
+            ?kv(K,_Val) = lists:last(Bkt),
+            {ok,K}
+    end.
 
--spec update(Key, Fun, Dict1) -> Dict2 when
+%% see foldl for details on why this goes from lower bucket numbers to higher
+prev_bucket(T, Slot) when Slot > T#dict.n -> [];
+prev_bucket(T, Slot) ->
+    case get_bucket(T, Slot) of
+ 	[] -> prev_bucket(T, Slot+1);		%Empty bucket
+ 	B -> B
+    end.
+
+take_last(Dict) ->
+    take(last_key(Dict), Dict).
+
+%% prev_key(Key, Table) -> {ok,PrevKey} | error.
+%% Find next key *before* Key in Table (in the order defined by foldl).
+
+prev_key(Key, T) ->
+    Slot = get_slot(T, Key),
+    B = get_bucket(T, Slot),
+    %% Find a bucket with something in it.
+    Bkt = case bucket_before_key(Key, B) of
+ 	      error -> erlang:error(badarg,[Key, T]);
+ 	      [] -> prev_bucket(T, Slot+1);
+ 	      Rest -> Rest
+ 	  end,
+    case Bkt of
+ 	[] -> error;				%We have reached the end!
+        Bkt ->
+            ?kv(Prev,_Val) = lists:last(Bkt),
+            {ok,Prev}
+    end.
+
+bucket_before_key(Key, [KV|Bkt]) ->
+    bucket_before_key(Key, Bkt, KV);
+bucket_before_key(_Key, []) -> error. %Key not found!
+
+bucket_before_key(Key, [?kv(Key,_Val)|_],KV) -> [KV]; % return as a bucket
+bucket_before_key(Key, [KV|Bkt],_PrevKV) ->
+    bucket_before_key(Key, Bkt, KV);
+bucket_before_key(_Key, [], _PrevKV) -> error. %Key not found!
+
+%% deprecated version of map/3
+update(Key, F, D0) ->
+    map(F, Key, D0).
+
+%% map fun to a particular key only, fail if key not present
+-spec map(Key, Fun, Dict1) -> Dict2 when
       Key :: term(),
       Fun :: fun((Value1 :: term()) -> Value2 :: term()),
       Dict1 :: dict(),
       Dict2 :: dict().
-
-update(Key, F, D0) ->
+map(F, Key, D0) ->
     Slot = get_slot(D0, Key),
     try on_bucket(fun (B0) -> update_bkt(Key, F, B0) end, D0, Slot) of
 	{D1,_Uv} -> D1
@@ -406,14 +466,18 @@ update_bkt(Key, F, [Other|Bkt0]) ->
 update_bkt(_Key, _F, []) ->
     throw(badarg).
 
--spec update(Key, Fun, Initial, Dict1) -> Dict2 when
+%% deprecated version of map/4
+update(Key, F, Init, D0) ->
+    map(F, Key, Init, D0).
+
+%% map fun to a particular key only, insert default if key not present
+-spec map(Key, Fun, Initial, Dict1) -> Dict2 when
       Key :: term(),
       Initial :: term(),
       Fun :: fun((Value1 :: term()) -> Value2 :: term()),
       Dict1 :: dict(),
       Dict2 :: dict().
-
-update(Key, F, Init, D0) ->
+map(F, Key, Init, D0) ->
     Slot = get_slot(D0, Key),
     {D1,Ic} = on_bucket(fun (B0) -> update_bkt(Key, F, Init, B0) end,
 			D0, Slot),
@@ -432,14 +496,15 @@ update_bkt(Key, F, I, []) when is_function(F, 1) -> {[?kv(Key,I)],1}.
       Dict1 :: dict(),
       Dict2 :: dict().
 
-update_counter(Key, Incr, D0) when is_number(Incr) ->
+%% deprecated version of increment/3
+increment(Incr, Key, D0) when is_number(Incr) ->
     Slot = get_slot(D0, Key),
     {D1,Ic} = on_bucket(fun (B0) -> counter_bkt(Key, Incr, B0) end,
 			D0, Slot),
     maybe_expand(D1, Ic).
 
-increment(Delta, Key, Dict) ->
-    update_counter(Key, Delta, Dict).
+update_counter(Key, Incr, D0) ->
+    increment(Incr, Key, D0).
 
 counter_bkt(Key, I, [?kv(Key,Val)|Bkt]) ->
     {[?kv(Key,Val+I)|Bkt],0};
@@ -447,104 +512,6 @@ counter_bkt(Key, I, [Other|Bkt0]) ->
     {Bkt1,Ic} = counter_bkt(Key, I, Bkt0),
     {[Other|Bkt1],Ic};
 counter_bkt(Key, I, []) -> {[?kv(Key,I)],1}.
-
--spec fold(Fun, Acc0, Dict) -> Acc1 when
-      Fun :: fun((Key, Value, AccIn) -> AccOut),
-      Key :: term(),
-      Value :: term(),
-      Acc0 :: term(),
-      Acc1 :: term(),
-      AccIn :: term(),
-      AccOut :: term(),
-      Dict :: dict().
-%%  Fold function Fun over all "bags" in Table and return Accumulator.
-
-fold(F, Acc, D) -> fold_dict(F, Acc, D).
-
-%% fold in default traversal order (not key order unless dict is ordered)
--spec foldl(Fun, Acc0, Dict) -> Acc1 when
-      Fun :: fun((Key, Value, AccIn) -> AccOut),
-      Key :: term(),
-      Value :: term(),
-      Acc0 :: term(),
-      Acc1 :: term(),
-      AccIn :: term(),
-      AccOut :: term(),
-      Dict :: dict().
-foldl(Fun, Acc, Dict) -> fold_dict(Fun, Acc, Dict).
-
-%% fold in reverse traversal order
-foldr(_Fun, _Acc, _Dict) -> throw('FIXME').
-
-%% uses first element as initial accumulator, fails if list is empty
--spec foldl1(Fun, Dict) -> Acc when
-      Fun :: fun((Key, Value, AccIn) -> AccOut),
-      Key :: term(),
-      Value :: term(),
-      Acc :: term(),
-      AccIn :: term(),
-      AccOut :: term(),
-      Dict :: dict().
-foldl1(_Fun, _Dict) -> throw('FIXME').
-
-%% uses last element as initial accumulator, fails if list is empty
-foldr1(_Fun, _Dict) -> throw('FIXME').
-
-%% applies InitFun to first element to get initial accumulator
--spec foldln(Fun, InitFun, Dict) -> Acc when
-      Fun :: fun((Key, Value, AccIn) -> AccOut),
-      InitFun :: fun((Key, Value) -> Acc0),
-      Key :: term(),
-      Value :: term(),
-      Acc :: term(),
-      Acc0 :: term(),
-      AccIn :: term(),
-      AccOut :: term(),
-      Dict :: dict().
-foldln(_Fun, _InitFun, _Dict) -> throw('FIXME').
-
-%% applies InitFun to last element to get initial accumulator
-foldrn(_Fun, _InitFun, _Dict) -> throw('FIXME').
-
--spec foreach(Fun, Dict) -> ok when
-      Fun :: fun((Key :: term(), Value :: term()) -> term()),
-      Dict :: dict().
-foreach(_Fun, _Dict) -> throw('FIXME').
-
-%% map fun to a particular key only, fail if key not present
-map(_Fun, _Key, _Dict) -> throw('FIXME').
-
-%% map fun to a particular key only, insert default if key not present
-map(_Fun, _Key, _Default, _Dict) -> throw('FIXME').
-
--spec map(Fun, Dict1) -> Dict2 when
-      Fun :: fun((Key :: term(), Value1 :: term()) -> Value2 :: term()),
-      Dict1 :: dict(),
-      Dict2 :: dict().
-
-map(F, D) -> map_dict(F, D).
-
--spec filter(Pred, Dict1) -> Dict2 when
-      Pred :: fun((Key :: term(), Value :: term()) -> boolean()),
-      Dict1 :: dict(),
-      Dict2 :: dict().
-
-filter(F, D) -> filter_dict(F, D).
-
--spec merge(Fun, Dict1, Dict2) -> Dict3 when
-      Fun :: fun((Key :: term(), Value1 :: term(), Value2 :: term()) -> Value :: term()),
-      Dict1 :: dict(),
-      Dict2 :: dict(),
-      Dict3 :: dict().
-
-merge(F, D1, D2) when D1#dict.size < D2#dict.size ->
-    fold_dict(fun (K, V1, D) ->
-		      update(K, fun (V2) -> F(K, V1, V2) end, V1, D)
-	      end, D2, D1);
-merge(F, D1, D2) ->
-    fold_dict(fun (K, V2, D) ->
-		      update(K, fun (V1) -> F(K, V1, V2) end, V2, D)
-	      end, D1, D2).
 
 
 %% get_slot(Hashdb, Key) -> Slot.
@@ -574,33 +541,107 @@ on_bucket(F, T, Slot) ->
     {B1,Res} = F(B0),				%Op on the bucket.
     {T#dict{segs=setelement(SegI, Segs, setelement(BktI, Seg, B1))},Res}.
 
-%% fold_dict(Fun, Acc, Dictionary) -> Acc.
-%% map_dict(Fun, Dictionary) -> Dictionary.
-%% filter_dict(Fun, Dictionary) -> Dictionary.
-%%
 %%  Work functions for fold, map and filter operations.  These
 %%  traverse the hash structure rebuilding as necessary.  Note we
 %%  could have implemented map and filter using fold but these are
 %%  faster.  We hope!
 
-fold_dict(F, Acc, D) ->
-    Segs = D#dict.segs,
-    fold_segs(F, Acc, Segs, tuple_size(Segs)).
+%%  Fold function Fun over all "bags" in Table and return Accumulator.
 
-fold_segs(F, Acc, Segs, I) when I >= 1 ->
+%% deprecated version of foldl
+fold(F, Acc, D) -> foldl(F, Acc, D).
+
+%% fold in default traversal order (not key order unless dict is ordered)
+-spec foldl(Fun, Acc0, Dict) -> Acc1 when
+      Fun :: fun((Key, Value, AccIn) -> AccOut),
+      Key :: term(),
+      Value :: term(),
+      Acc0 :: term(),
+      Acc1 :: term(),
+      AccIn :: term(),
+      AccOut :: term(),
+      Dict :: dict().
+foldl(Fun, Acc, Dict) ->
+    Segs = Dict#dict.segs,
+    foldl_segs(Fun, Acc, Segs, tuple_size(Segs)).
+
+%% Note that this traversal *defines* what the default order is - even if it
+%% traverses the individual tuples from right to left, it's still "foldl"!
+%% This order must match the functions first_key/last_key/next_key/prev_key!
+%% (Rather than change the legacy traversal order of the default fold, we
+%% have chosen to implement first/last/next/prev to go from higher bucket
+%% indices towards lower, just like foldl.)
+foldl_segs(F, Acc, Segs, I) when I >= 1 ->
     Seg = element(I, Segs),
-    fold_segs(F, fold_seg(F, Acc, Seg, tuple_size(Seg)), Segs, I-1);
-fold_segs(F, Acc, _, 0) when is_function(F, 3) -> Acc.
+    foldl_segs(F, foldl_seg(F, Acc, Seg, tuple_size(Seg)), Segs, I-1);
+foldl_segs(F, Acc, _, 0) when is_function(F, 3) -> Acc.
 
-fold_seg(F, Acc, Seg, I) when I >= 1 ->
-    fold_seg(F, fold_bucket(F, Acc, element(I, Seg)), Seg, I-1);
-fold_seg(F, Acc, _, 0) when is_function(F, 3) -> Acc.
+foldl_seg(F, Acc, Seg, I) when I >= 1 ->
+    foldl_seg(F, foldl_bucket(F, Acc, element(I, Seg)), Seg, I-1);
+foldl_seg(F, Acc, _, 0) when is_function(F, 3) -> Acc.
 
-fold_bucket(F, Acc, [?kv(Key,Val)|Bkt]) ->
-    fold_bucket(F, F(Key, Val, Acc), Bkt);
-fold_bucket(F, Acc, []) when is_function(F, 3) -> Acc.
+foldl_bucket(F, Acc, [?kv(Key,Val)|Bkt]) ->
+    foldl_bucket(F, F(Key, Val, Acc), Bkt);
+foldl_bucket(F, Acc, []) when is_function(F, 3) -> Acc.
 
-map_dict(F, D) ->
+%% fold in reverse traversal order
+foldr(Fun, Acc, Dict) ->
+    Segs = Dict#dict.segs,
+    foldr_segs(Fun, Acc, Segs, 1, tuple_size(Segs)).
+
+foldr_segs(F, Acc, Segs, I, N) when I =< N ->
+    Seg = element(I, Segs),
+    foldr_segs(F, foldr_seg(F, Acc, Seg, 1, tuple_size(Seg)), Segs, I+1, N);
+foldr_segs(F, Acc, _, _I, _N) when is_function(F, 3) -> Acc.
+
+foldr_seg(F, Acc, Seg, I, N) when I =< N ->
+    foldr_seg(F, foldr_bucket(F, Acc, element(I, Seg)), Seg, I+1, N);
+foldr_seg(F, Acc, _, _I, _N) when is_function(F, 3) -> Acc.
+
+foldr_bucket(F, Acc, [?kv(Key,Val)|Bkt]) ->
+    F(Key, Val, foldr_bucket(F, Acc, Bkt));
+foldr_bucket(F, Acc, []) when is_function(F, 3) -> Acc.
+
+%% uses first element as initial accumulator, fails if list is empty
+-spec foldl1(Fun, Dict) -> Acc when
+      Fun :: fun((Key, Value, AccIn) -> AccOut),
+      Key :: term(),
+      Value :: term(),
+      Acc :: term(),
+      AccIn :: term(),
+      AccOut :: term(),
+      Dict :: dict().
+foldl1(Fun, Dict) ->
+    foldl(Fun, take_first(Dict), Dict).
+
+%% uses last element as initial accumulator, fails if list is empty
+foldr1(Fun, Dict) ->
+    foldr(Fun, take_last(Dict), Dict).
+
+%% applies InitFun to first element to get initial accumulator
+-spec foldln(Fun, InitFun, Dict) -> Acc when
+      Fun :: fun((Key, Value, AccIn) -> AccOut),
+      InitFun :: fun((Key, Value) -> Acc0),
+      Key :: term(),
+      Value :: term(),
+      Acc :: term(),
+      Acc0 :: term(),
+      AccIn :: term(),
+      AccOut :: term(),
+      Dict :: dict().
+foldln(Fun, InitFun, Dict) ->
+    foldl(Fun, InitFun(take_first(Dict)), Dict).
+
+%% applies InitFun to last element to get initial accumulator
+foldrn(Fun, InitFun, Dict) ->
+    foldr(Fun, InitFun(take_last(Dict)), Dict).
+
+-spec map(Fun, Dict1) -> Dict2 when
+      Fun :: fun((Key :: term(), Value1 :: term()) -> Value2 :: term()),
+      Dict1 :: dict(),
+      Dict2 :: dict().
+
+map(F, D) ->
     Segs0 = tuple_to_list(D#dict.segs),
     Segs1 = map_seg_list(F, Segs0),
     D#dict{segs=list_to_tuple(Segs1)}.
@@ -619,7 +660,12 @@ map_bucket(F, [?kv(Key,Val)|Bkt]) ->
     [?kv(Key,F(Key, Val))|map_bucket(F, Bkt)];
 map_bucket(F, []) when is_function(F, 2) -> [].
 
-filter_dict(F, D) ->
+-spec filter(Pred, Dict1) -> Dict2 when
+      Pred :: fun((Key :: term(), Value :: term()) -> boolean()),
+      Dict1 :: dict(),
+      Dict2 :: dict().
+
+filter(F, D) ->
     Segs0 = tuple_to_list(D#dict.segs),
     {Segs1,Fc} = filter_seg_list(F, Segs0, [], 0),
     maybe_contract(D#dict{segs=list_to_tuple(Segs1)}, Fc).
@@ -644,6 +690,27 @@ filter_bucket(F, [?kv(Key,Val)=E|Bkt], Fb, Fc) ->
     end;
 filter_bucket(F, [], Fb, Fc) when is_function(F, 2) ->
     {lists:reverse(Fb),Fc}.
+
+-spec foreach(Fun, Dict) -> ok when
+      Fun :: fun((Key :: term(), Value :: term()) -> term()),
+      Dict :: dict().
+foreach(Fun, Dict) ->
+    foldl(fun (Key, Val, _Acc) -> Fun(Key, Val), ok end, ok, Dict).
+
+-spec merge(Fun, Dict1, Dict2) -> Dict3 when
+      Fun :: fun((Key :: term(), Value1 :: term(), Value2 :: term()) -> Value :: term()),
+      Dict1 :: dict(),
+      Dict2 :: dict(),
+      Dict3 :: dict().
+
+merge(F, D1, D2) when D1#dict.size < D2#dict.size ->
+    foldl(fun (K, V1, D) ->
+                  map(fun (V2) -> F(K, V1, V2) end, K, V1, D)
+          end, D2, D1);
+merge(F, D1, D2) ->
+    foldl(fun (K, V2, D) ->
+                  map(fun (V1) -> F(K, V1, V2) end, K, V2, D)
+          end, D1, D2).
 
 %% get_bucket_s(Segments, Slot) -> Bucket.
 %% put_bucket_s(Segments, Slot, Bucket) -> NewSegments.
